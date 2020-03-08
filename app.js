58c65de05d804b13dfe979b94cbb9dd5b5a0e5bf
Ritik <48304564+Ritik008@users.noreply.github.com>
2020-03-09 00:44:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,14 +21,14 @@ let puzzle = document.createElement("p");
 puzzle.textContent = game1.puzzle;
 let gusses = document.createElement("p");
 gusses.textContent = game1.statusMessage;
-document.querySelector("body").appendChild(puzzle);
-document.querySelector("body").appendChild(gusses);
+document.querySelector(".game").appendChild(puzzle);
+document.querySelector(".game").appendChild(gusses);
 
 window.addEventListener("keypress", function(e) {
 	const guess = String.fromCharCode(e.charCode);
 	game1.makeGuess(guess);
 	puzzle.textContent = game1.puzzle;
 	gusses.textContent = game1.statusMessage;
-	document.querySelector("body").appendChild(puzzle);
-	document.querySelector("body").appendChild(gusses);
+	document.querySelector("game").appendChild(puzzle);
+	document.querySelector("game").appendChild(gusses);
 });
